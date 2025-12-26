669c0e2b1675e19abf993cc39d9e5cb6f01f184f
Sachin Hadimani <sachina0075@gmail.com>
2025-12-26 16:26:09 +0530

    vite to webpack to setup and also babel

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
-import CartProvider from './context/CartContext';  // Correct import
+import App from '../src/App';
+import CartProvider from './context/CartContext.jsx';  // Correct import
 
 
 createRoot(document.getElementById('root')).render(

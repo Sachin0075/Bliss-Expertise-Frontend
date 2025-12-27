0a10b38b09d3e5ac84d0570475463742fade6f1f
Sachin Hadimani <133744770+Sachin0075@users.noreply.github.com>
2025-12-28 00:52:53 +0530

    Update webpack.prod.cjs

--- webpack/webpack.prod.cjs
+++ webpack/webpack.prod.cjs
@@ -7,6 +7,12 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
         }),
-        new BundleAnalyzePlugin(),
+        // In CI we must not launch an interactive analyzer server because it blocks the build.
+        // Generate a static report instead and don't open the analyzer.
+        new BundleAnalyzePlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+            reportFilename: 'bundle-report.html',
+        }),
     ],
 }
